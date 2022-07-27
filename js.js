0a4f436a7d6e29608e253ddd6865e72ffc27f94e
Ijasgit <mohammedijasn3899@gmail.com>
2022-07-27 16:25:47 +0530

    .

---
+++ js.js
@@ -0,0 +1,15 @@
+function load(){
+    let b=document.querySelectorAll('#arun');
+    let x="IJas";
+    let y=x.split('');
+    console.log(y);
+    b.forEach((a)=>{
+        let ran=Math.floor(Math.random()*y.length);
+        a.innerHTML=y[ran];
+        y.splice(ran,1);
+        console.log(ran);
+       
+    })
+}
+// var load=document.getElementById('sf')
+// load.addEventListerner('click',lo)
